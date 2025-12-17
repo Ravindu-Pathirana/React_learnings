aa77c93df04b047e41b32425205e6e1b85a439b2
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2025-12-18 00:17:42 +0530

    Es modules

---
+++ T12 - Es modules/extra.js
@@ -0,0 +1,5 @@
+export const text ='This is some extra text from extra.js file';
+
+export function hello(name){
+    console.log(`Hello ${name}`);
+}
---
+++ T12 - Es modules/index.js
@@ -0,0 +1,5 @@
+import {text as extra, hello} from './extra.js';
+
+hello('Alice');
+
+console.log(extra);
