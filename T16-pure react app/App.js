f8d34e5a72d79dfc94a1f101ac5ebe666c83cf12
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2025-12-17 19:05:21 +0530

    Update App.js

--- T16-pure react app/App.js
+++ T16-pure react app/App.js
@@ -1,19 +1,19 @@
-const student = ()=>{
+const student = (student_details)=>{
     return React.createElement(
         "div",
         {},
-        [React.createElement("h3",{},"Ravindu"),
-        React.createElement("h3",{},"22 years old"),
-        React.createElement("h3",{},"University of Moratuwa")]
+        [React.createElement("h3",{},student_details.name),
+        React.createElement("h3",{},student_details.age + " years old"),
+        React.createElement("h3",{},student_details.university)]
     );
 };
 
-const studentcard =()=>{
+const studentcard =(student_details)=>{
     return React.createElement(
         "div",
         {className:"st-card"},
-        React.createElement("img",{className:"student-image", src:"https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png", alt:"student-image"}),
-        React.createElement(student)
+        React.createElement("img",{className:"student-image", src:student_details.image, alt:"student-image"}),
+        React.createElement(student, {name: student_details.name, age: student_details.age, university: student_details.university})
     );
 }
 
@@ -22,10 +22,30 @@ const App = ()=>{
                 "div",
                 {},
                 [React.createElement("h1",{},"My React App"),
-                React.createElement(studentcard),
-                React.createElement(studentcard),
-                React.createElement(studentcard),
-                React.createElement(studentcard),   
+                React.createElement(studentcard, {
+                    name: "Ravindu",
+                    age: 22,
+                    university: "University of Moratuwa",
+                    image: "https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png"
+                }),
+                React.createElement(studentcard, {
+                    name: "Sarah",
+                    age: 21,
+                    university: "University of Colombo",
+                    image: "https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png"
+                }),
+                React.createElement(studentcard, {
+                    name: "Michael",
+                    age: 23,
+                    university: "University of Peradeniya",
+                    image: "https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png",
+                }),
+                React.createElement(studentcard, {
+                    name: "Amaya",
+                    age: 20,
+                    university: "University of Kelaniya",
+                    image: "https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png"
+                })
                 ]                
             );
 };
