424bec924399ec7645bc9942ecb9c1cfa0905dde
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2025-12-20 10:41:16 +0530

    Prop Types

---
+++ T25-Prop Types/my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import StudentCard from "/Volumes/My Disk 2/React/React_learnings/T25-Prop Types/my-react-app/src/components/StudentCard.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <StudentCard name="Ravindu" age={22} university="University of Moratuwa" />
+      <StudentCard name="Kamal" age={21} university="University of Kelaniya" />
+      <StudentCard name="Pasindu" age={20} university="University of Colombo" />
+      <StudentCard name="Yarn" age={24} university="University of J'Pura" />
+      <StudentCard name="Anne" age={20} university="University of Peradeniya" />
+    </div>
+  );
+}
+
+export default App;
