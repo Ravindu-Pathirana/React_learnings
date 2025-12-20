65b1fa7ba0c0520432f4cc5ad7673604ce08ac5e
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2025-12-20 07:34:52 +0530

    updated

--- T24-Create React App using JavaScript/my-react-app/src/App.test.js
+++ T24-Create React App using JavaScript/my-react-app/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '/Volumes/My Disk 2/React/React_learnings/T24-Create React App using Javascript/my-react-app/src/App.js';
 
 test('renders learn react link', () => {
   render(<App />);
